da4b5705bdddb5fe3bc287715f2739144c842d34
Lucas J. Chumley <lucas@chumley.io>
2022-10-29 19:28:25 -0400

    Initial Commit

---
+++ UkoziMD/UkoziMDApp.swift
@@ -0,0 +1,17 @@
+//
+//  UkoziMDApp.swift
+//  UkoziMD
+//
+//  Created by Lucas J. Chumley on 10/29/22.
+//
+
+import SwiftUI
+
+@main
+struct UkoziMDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
